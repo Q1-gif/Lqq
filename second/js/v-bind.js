a52bccb732456962fa1421e66535dcb859454ef0
Q1-gif <33432172857@qq.com>
2019-09-16 09:10:42 +0800

    提交第二次文件

---
+++ second/js/v-bind.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        url:"https://huaban.com/",
+        img:"img/logo.png",
+        VClass:"bg "
+    }
+})
---
+++ second/js/v-for.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el:"#app",
+    data:{
+        foodList:[{
+            name:"炸鸡",
+            price:20,
+            discount:0.9
+        },
+        {
+        name:"可乐",
+        price:6,
+        discount:0.9
+        },
+        {
+        name:"薯条",
+        price:8,
+        discount:null
+        },
+        ]}
+})
